7c0414a4697f94c2f531110dd275a35fb9f777c4
Fadia Nufinikita <103888296+fadiaann26@users.noreply.github.com>
2023-10-04 14:20:00 +0700

    Add files via upload

---
+++ datalist.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<int> datalistku = [4, 7, 8, 2, 6, 3, 6, 78];
+
+  // Mengurutkan dalam urutan naik (ascending)
+  datalistku.sort();
+
+  print('Data yang telah diurutkan dalam urutan naik: $datalistku');
+}
+
+
---
+++ datalist2.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<int> datalistku = [4, 7, 8, 2, 6, 3, 6, 78];
+
+  // Menambahkan angka 109 ke dalam daftar
+  datalistku.add(109);
+
+  print('Data setelah menambahkan angka 109: $datalistku');
+}
